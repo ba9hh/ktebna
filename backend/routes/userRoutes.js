9ac3ca66a658acc897d91a70595dafc233eb25df
ba9hh <zedtourheart@gmail.com>
2025-08-30 14:16:52 +0100

    Add new feature X / Fix bug Y

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { fetchUserInformation, updateUserProfilePicture, updateUserName } = require("../controllers/userControllers");
-const router = express.Router();
-const authMiddleware = require("../authMiddleware.js");
-router.get("/user", authMiddleware, fetchUserInformation);
-router.put("/user/profile-picture", authMiddleware, updateUserProfilePicture);
-router.put("/user/name", authMiddleware, updateUserName);
-
-module.exports = router;
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { fetchUserInformation, updateUserProfilePicture, updateUserName } = require("../controllers/userControllers.js");
+const router = express.Router();
+const authMiddleware = require("../authMiddleware.js");
+router.get("/user", authMiddleware, fetchUserInformation);
+router.put("/user/profile-picture", authMiddleware, updateUserProfilePicture);
+router.put("/user/name", authMiddleware, updateUserName);
+
+module.exports = router;
